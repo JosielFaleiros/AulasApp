ba97232ecf101c2f72876e8502de301e85699c64
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2018-06-11 21:54:07 -0300

    Added base classes and activities #1

---
+++ app/src/main/java/mobile/aulasapp/com/aulasapp/model/Class.java
@@ -0,0 +1,20 @@
+package mobile.aulasapp.com.aulasapp.model;
+
+public class Class {
+    String name;
+
+    public Class() {
+    }
+
+    public Class(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
